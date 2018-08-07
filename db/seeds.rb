c856d6df30ab8e2fe72a0cccfd58bc29b86ff53f
Enzo Naguiat <enzo_naguiat@yahoo.com>
2018-08-07 11:04:52 +0800

    created seed files

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,39 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+puts "generating blog posts.."
+10.times do |blog|
+  Blog.create!(
+    title: "My Blog Post #{blog + 1}",
+    body: "Bacon ipsum dolor amet fatback short ribs ribeye
+           landjaeger burgdoggen cupim biltong turducken ground
+           round flank. Picanha pork hamburger corned beef ham.
+           Chicken tri-tip corned beef pork loin doner picanha
+           cupim pork belly kevin. Burgdoggen filet mignon
+           alcatra ribeye pastrami, beef ribs bacon biltong buffalo
+           turkey doner ball tip."
+  )
+end
+puts "generated blog posts"
+
+puts "generating skills..."
+5.times do |skill|
+  Skill.create!(
+    title: "Rails #{skill + 1}",
+    percent_utilized: 15
+  )
+end
+puts "completed generating skills"
+
+puts "generating projects..."
+6.times do |project|
+  Project.create!(
+    title: "Project #{project + 1}",
+    subtitle: "My great service.",
+    body: "Awesome",
+    main_image: "http://placehold.it/600x450",
+    thumb_image: "http://placehold.it/400x300"
+  )
+end
+puts "completed generating projects"
