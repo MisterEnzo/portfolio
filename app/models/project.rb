45ea0eff53f6cdf561a906576f37492670e25b43
Enzo Naguiat <enzo_naguiat@yahoo.com>
2018-08-13 10:41:25 +0800

    sets default main and thumb image of project items

--- app/models/project.rb
+++ app/models/project.rb
@@ -1,3 +1,10 @@
 class Project < ApplicationRecord
   validates_presence_of :title, :body, :main_image, :thumb_image
+
+  after_initialize :set_defaults
+
+  def set_defaults
+    self.main_image ||= "http://placehold.it/600x450"
+    self.thumb_image ||= "http://placehold.it/400x300"
+  end
 end
