ec9ef4fc9f218fe3d7a3965b273a554b36a1751d
Enzo Naguiat <enzo_naguiat@yahoo.com>
2018-09-22 21:09:02 +0800

    convert session[source] greeting into a helper method

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,8 @@
 module ApplicationHelper
+  def source_helper
+    if session[:source]
+      greeting = "Thanks for visiting me from #{session[:source]}"
+      content_tag(:p, greeting, class: "source-greeting")
+    end
+  end
 end
