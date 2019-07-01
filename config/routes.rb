14e880a4a7cf0dd573771889db0907e8e8277966
enz <enzo_naguiat@yahoo.com>
2019-07-01 17:53:38 +0900

    edit about page to remove picture, add github and linkedin links, and justify content. also removed contact page

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,8 @@
 Rails.application.routes.draw do
   devise_for :users, path: '', path_names: { sign_in: 'login', sign_out: 'logout', sign_up: 'register' }
-  
+
   resources :topics, only: [:index, :show]
-  
+
   resources :blogs do
     member do
       get :toggle_status
@@ -17,6 +17,6 @@ Rails.application.routes.draw do
 
   root to: 'pages#about'
   get '/about',    to:'pages#about'
-  get '/contact',  to:'pages#contact'
+  # get '/contact',  to:'pages#contact'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
